717c5c5c5842d9105b8f7d6eb3af419361fa0142
Enzo R <63020574+NamitasCl@users.noreply.github.com>
2023-10-08 22:06:00 -0300

    Finalizar implementacion de operaciones matematicas y comportamiento de funciones de la calculadora.

--- js.js
+++ js.js
@@ -1,8 +1,93 @@
-const numeros = document.querySelectorAll('#numero');
-const pantalla = document.querySelector('.pantalla');
+let borrarPantalla = false;
+let operador = null;
+let numeroUno = '';
+let numeroDos = '';
 
-numeros.forEach(num => {
-    num.addEventListener('click', (e) =>{
-        pantalla.textContent += e.target.textContent;        
-    });
+const numeros = document.querySelectorAll('#numero');
+const operadores = document.querySelectorAll('.operador')
+const pantalla = document.querySelector('.pantalla');
+const btnBorrar = document.querySelector('.borrar');
+const btnIgual = document.querySelector('.igual');
+const btnPunto = document.querySelector('.punto');
+const btnAc = document.querySelector('.cero');
+const btnDel = document.querySelector('.borrar');
+
+btnIgual.addEventListener('click', evaluar);
+btnPunto.addEventListener('click', agregaPunto);
+btnAc.addEventListener('click', borrarTodo);
+btnDel.addEventListener('click', borrarNumero);
+
+
+
+numeros.forEach(numero => {
+    numero.addEventListener('click', () => {setNumero(numero.textContent)});
 });
+
+operadores.forEach(operador => {
+    operador.addEventListener('click', (e) => {setOperador(e.target.id)});
+});
+
+function setNumero(numero) {
+    //sirve para no añadir ceros al inicio.
+    if(pantalla.textContent == '0' || borrarPantalla) delScrn();
+    pantalla.textContent += numero;
+}
+
+function setOperador(op) {
+    if(operador !== null) evaluar();
+    numeroUno = pantalla.textContent;
+    operador = op;
+    borrarPantalla = true;
+}
+
+function delScrn() {
+    pantalla.textContent = '';
+    borrarPantalla = false;
+}
+
+function evaluar() {
+    if(operador === null || borrarPantalla) return;
+    numeroDos = pantalla.textContent;
+    pantalla.textContent = operar(operador, numeroUno, numeroDos);
+    operador = null;
+}
+
+function agregaPunto() {
+    if(pantalla.textContent.includes('.')) return;
+    setNumero('.');
+}
+
+function borrarTodo() {
+    pantalla.textContent = '';
+    numeroUno = '';
+    numeroDos = '';
+    operador = null;
+}
+
+function borrarNumero() {
+    let aux = pantalla.textContent.split('');
+    aux.pop();
+    pantalla.textContent = aux.join('');
+}
+
+
+function operar(op, x, y) {
+    let a = Number(x);
+    let b = Number(y);
+
+    switch(op) {
+        case 'suma':
+            return a + b;
+            break;
+        case 'resta':
+            return a - b;
+            break;
+        case 'multiplicacion':
+            return a * b;
+            break;
+        case 'division':
+            return a / b;
+            break;
+    }   
+}
+
